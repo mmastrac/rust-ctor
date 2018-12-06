b7e62fab9bbec155cb15742d29225f37e239f173
Matt Mastracci <matthew@mastracci.com>
2018-12-05 23:01:52 -0700

    Check support for multiple ctor functions, update docs

--- ctor/src/lib.rs
+++ ctor/src/lib.rs
@@ -1,3 +1,5 @@
+//! Procedural macro for defining module startup functions.
+
 extern crate proc_macro;
 extern crate syn;
 #[macro_use]
@@ -5,6 +7,60 @@ extern crate quote;
 
 use proc_macro::TokenStream;
 
+/// Marks a function as a library/executable constructor. This uses OS-specific
+/// linker sections to call a specific function at load time.
+///
+/// Multiple startup functions are supported, but the invocation order is not
+/// guaranteed.
+///
+/// # Examples
+///
+/// Print a startup message:
+///
+/// ```rust
+/// # extern crate ctor;
+/// # use ctor::*;
+/// #[ctor]
+/// fn foo() {
+///   println!("Hello, world!");
+/// }
+///
+/// # fn main() {
+/// println!("main()");
+/// # }
+/// ```
+///
+/// Make changes to `static` variables:
+///
+/// ```rust
+/// # extern crate ctor;
+/// # use ctor::*;
+/// # use std::sync::atomic::{AtomicBool, Ordering, ATOMIC_BOOL_INIT};
+/// static INITED: AtomicBool = ATOMIC_BOOL_INIT;
+///
+/// #[ctor]
+/// fn foo() {
+///   INITED.store(true, Ordering::SeqCst);
+/// }
+/// ```
+///
+/// # Details
+///
+/// The `#[ctor]` macro makes use of linker sections to ensure that a
+/// function is run at startup time.
+///
+/// The above example translates into the following Rust code (approximately):
+///
+///```rust,ignore
+/// #[used]
+/// #[cfg_attr(target_os = "linux", link_section = ".ctors")]
+/// #[cfg_attr(target_os = "macos", link_section = "__DATA,__mod_init_func")]
+/// #[cfg_attr(target_os = "windows", link_section = ".CRT$XCU")]
+/// static foo: extern fn() = {
+///   extern fn foo() { ... };
+///   foo
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn ctor(_attribute: TokenStream, function: TokenStream) -> TokenStream {
     let output: syn::Item = syn::parse(function).unwrap();
@@ -44,7 +100,7 @@ pub fn ctor(_attribute: TokenStream, function: TokenStream) -> TokenStream {
             #[cfg_attr(target_os = "macos", link_section = "__DATA,__mod_init_func")]
             #[cfg_attr(target_os = "windows", link_section = ".CRT$XCU")]
             #(#attrs)*
-            pub static #ident
+            static #ident
             :
             extern #unsafety #abi #constness fn() =
             { extern #unsafety #abi #constness fn #ident() #block; #ident }
--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -7,6 +7,7 @@ mod test {
     use std::sync::atomic::{AtomicBool, Ordering, ATOMIC_BOOL_INIT};
 
     static INITED: AtomicBool = ATOMIC_BOOL_INIT;
+    static INITED_2: AtomicBool = ATOMIC_BOOL_INIT;
 
     /// Doc comment
     #[ctor]
@@ -14,9 +15,16 @@ mod test {
         INITED.store(true, Ordering::SeqCst);
     }
 
+    /// This ensures that we support more than of these
+    #[ctor]
+    fn foo_2() {
+        INITED_2.store(true, Ordering::SeqCst);
+    }
+
     #[test]
     fn test_initialized() {
         // Test to see that the ctor ran
         assert_eq!(true, INITED.load(Ordering::SeqCst));
+        assert_eq!(true, INITED_2.load(Ordering::SeqCst));
     }
 }
