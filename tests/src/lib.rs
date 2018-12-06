2a331e06b9c008623d6bcc2c09f3a7a401015208
Matt Mastracci <matthew@mastracci.com>
2018-12-06 10:14:58 -0700

    Split Windows dtor test func out

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -21,11 +21,20 @@ mod test {
         INITED_2.store(true, Ordering::SeqCst);
     }
 
+    #[cfg(not(target_os = "windows"))]
     #[dtor]
-    unsafe fn shutdown() {
+    unsafe fn shutdown_unix() {
         // Using println or eprintln here will panic as Rust has shut down
         let msg = "We don't test shutdown, but if you see this message it worked!\n";
         libc::write(1, std::mem::transmute(msg.as_ptr()), msg.len() as libc::size_t);
+    }
+
+    #[cfg(target_os = "windows")]
+    #[dtor]
+    unsafe fn shutdown_windows() {
+        // Using println or eprintln here will panic as Rust has shut down
+        let msg = "We don't test shutdown, but if you see this message it worked!\n";
+        libc::write(1, std::mem::transmute(msg.as_ptr()), msg.len() as u32);
     }
 
     #[test]
