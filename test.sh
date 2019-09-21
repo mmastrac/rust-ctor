7cf6299862b4727bbb559177bcb4ea85701d31a4
Matt Mastracci <matthew@mastracci.com>
2019-09-20 20:39:31 -0600

    musl test

--- test.sh
+++ test.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -euf -o pipefail
+
+if [[ $MUSL == "true" ]]; then
+
+# We can't test dynamic linking on musl
+echo "Testing --target x86_64-unknown-linux-musl..."
+cargo run --example example --target x86_64-unknown-linux-musl
+
+else
+
 cargo test --all
 cargo test --all --release
 
@@ -7,4 +16,6 @@ if [[ $TRAVIS_OS_NAME == "linux" && $TRAVIS_RUST_VERSION == "nightly" ]]; then
     echo "Testing '-Z sanitizer=address'..."
     # Target needed to avoid "Only executables, staticlibs, cdylibs, dylibs and rlibs can be compiled..."
     RUSTFLAGS="-Z sanitizer=address" cargo run --example example --target x86_64-unknown-linux-gnu
+fi
+
 fi
