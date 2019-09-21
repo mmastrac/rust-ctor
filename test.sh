ee0fd4d62ea5ecf2ff3483ed8e85a8c27abcf6f8
Matt Mastracci <matthew@mastracci.com>
2019-09-20 19:40:13 -0600

    ./test.sh

--- test.sh
+++ test.sh
@@ -2,3 +2,8 @@
 set
 cargo test --all
 cargo test --all --release
+
+if [[ $TRAVIS_OS_NAME == "linux" && $TRAVIS_RUST_VERSION == "nightly" ]]; then
+    echo "Testing '-Z sanitizer=address'..."
+    RUSTFLAGS="-Z sanitizer=address" cargo run --example example
+fi
