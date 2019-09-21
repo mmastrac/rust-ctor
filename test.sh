c3c77a5b65366c969d9e8f3ea1f127bd8ea1a1f0
Matt Mastracci <matthew@mastracci.com>
2019-09-20 20:27:25 -0600

    ./test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ cargo test --all --release
 
 if [[ $TRAVIS_OS_NAME == "linux" && $TRAVIS_RUST_VERSION == "nightly" ]]; then
     echo "Testing '-Z sanitizer=address'..."
-    cargo run --example example
-    RUSTFLAGS="-Z sanitizer=address" cargo run --example example
+    # Target needed to avoid "Only executables, staticlibs, cdylibs, dylibs and rlibs can be compiled..."
+    RUSTFLAGS="-Z sanitizer=address" cargo run --example example --target x86_64-unknown-linux-gnu
 fi
