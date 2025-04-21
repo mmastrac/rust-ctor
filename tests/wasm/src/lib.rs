562472a6f278d384d517b41e13ca4a371fe0db0f
CPunisher <1343316114@qq.com>
2025-04-22 01:54:12 +0800

    Support wasm without wasm_bindgen (#337)
    Implementation thanks to @CPunisher.

--- tests/wasm/src/lib.rs
+++ tests/wasm/src/lib.rs
@@ -1,13 +1,14 @@
-#[cfg(target_family = "wasm")]
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+static STATE: AtomicUsize = AtomicUsize::new(0);
+
 #[ctor::ctor]
-unsafe fn hello() {
-    use wasm_bindgen::prelude::wasm_bindgen;
+pub fn init() {
+    STATE.fetch_add(1, Ordering::Relaxed);
+}
 
-    #[wasm_bindgen]
-    extern "C" {
-        #[wasm_bindgen(js_namespace = console)]
-        fn log(s: &str);
-    }
-
-    log("hello!");
+#[cfg(target_family = "wasm")]
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() {
+    assert_eq!(STATE.load(Ordering::Relaxed), 1);
 }
