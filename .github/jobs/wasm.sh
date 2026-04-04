383702d145c7b73f155e01025eb97d7536fd16c7
Matt Mastracci <matthew@mastracci.com>
2026-04-04 10:20:33 -0600

    Clean up workspace

--- .github/jobs/wasm.sh
+++ .github/jobs/wasm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rustup target add wasm32-unknown-unknown
-cargo build --target wasm32-unknown-unknown -p tests-wasm
+cd tests/ctor/wasm
+cargo build --target wasm32-unknown-unknown
 wasmtime run target/wasm32-unknown-unknown/debug/tests_wasm.wasm
