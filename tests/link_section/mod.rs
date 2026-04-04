cd2f22a336b313ea318484ec9c91b516a10ae4e2
Matt Mastracci <matthew@mastracci.com>
2026-04-04 17:45:56 -0600

    Test macOs

--- tests/link_section/mod.rs
+++ tests/link_section/mod.rs
@@ -11,7 +11,7 @@ $ cargo build --quiet
 $ cargo run --quiet
 ! LINK_SECTION: Section { name: "__TEXT,LINK_SECTION", start: %{BASE16NUM}, end: %{BASE16NUM}, byte_len: 40 }
 ! link_section_function
-! TYPED_LINK_SECTION: TypedSection { name: "__DATA,TYPED_LINKIXOI_M", start: 0x100c74268, end: %{BASE16NUM}, len: 2, stride: 4 }
+! TYPED_LINK_SECTION: TypedSection { name: "__DATA,TYPED_LINKIXOI_M", start: %{BASE16NUM}, end: %{BASE16NUM}, len: 2, stride: 4 }
 ! CODE_SECTION: TypedSection { name: "__DATA,FN_ARRAY", start: %{BASE16NUM}, end: %{BASE16NUM}, len: 3, stride: 8 }
 ! [%{BASE16NUM}, %{BASE16NUM}, %{BASE16NUM}]
 ! f: %{BASE16NUM}
