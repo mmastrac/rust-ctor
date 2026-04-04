2a25271b5b3daadca8853b6aa3806eeefab72721
Matt Mastracci <matthew@mastracci.com>
2026-04-04 12:12:48 -0600

    Move to clitest for integration tests

---
+++ tests/ctor/mod.rs
@@ -0,0 +1,50 @@
+use clitest_lib::clitest;
+
+clitest!(edition_2018, r#"
+cd "ctor/edition-2018";
+$ cargo build --quiet
+*
+$ cargo run --quiet
+! foo
+! main
+"#);
+
+clitest!(edition_2021, r#"
+cd "ctor/edition-2021";
+$ cargo build --quiet
+*
+$ cargo run --quiet
+! foo
+! main
+"#);
+
+clitest!(edition_2024, r#"
+cd "ctor/edition-2024";
+$ cargo build --quiet
+*
+$ cargo run --quiet
+! foo
+! main
+"#);
+
+clitest!(no_std, r#"
+cd "ctor/no-std";
+$ cargo build --quiet
+*
+"#);
+
+clitest!(system, r#"
+cd "ctor/system";
+$ cargo build --lib --examples --quiet
+*
+$ cargo run --example dylib_load --quiet
+"""
++ ctor bin
+++ main start
++++ ctor STATIC_INT
++++ ctor lib (-crt-static)
+-- main end
+--- dtor lib
+- dtor bin
+"""
+"#);
