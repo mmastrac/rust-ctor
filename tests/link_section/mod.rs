2cea713c6f19dbb3dffd7273a6fefe2ce43a6560
Matt Mastracci <matthew@mastracci.com>
2026-04-04 17:41:45 -0600

    %{BASE16NUM}

--- tests/link_section/mod.rs
+++ tests/link_section/mod.rs
@@ -9,15 +9,15 @@ defer {
 $ cargo build --quiet
 *
 $ cargo run --quiet
-! LINK_SECTION: Section { name: "__TEXT,LINK_SECTION", start: 0x100c600e8, end: 0x100c60110, byte_len: 40 }
+! LINK_SECTION: Section { name: "__TEXT,LINK_SECTION", start: %{BASE16NUM}, end: %{BASE16NUM}, byte_len: 40 }
 ! link_section_function
-! TYPED_LINK_SECTION: TypedSection { name: "__DATA,TYPED_LINKIXOI_M", start: 0x100c74268, end: 0x100c74270, len: 2, stride: 4 }
-! CODE_SECTION: TypedSection { name: "__DATA,FN_ARRAY", start: 0x100c74220, end: 0x100c74238, len: 3, stride: 8 }
-! [0x100c600e8, 0x100c29868, 0x100c29890]
-! f: 0x100c600e8
-! f: 0x100c29868
+! TYPED_LINK_SECTION: TypedSection { name: "__DATA,TYPED_LINKIXOI_M", start: 0x100c74268, end: %{BASE16NUM}, len: 2, stride: 4 }
+! CODE_SECTION: TypedSection { name: "__DATA,FN_ARRAY", start: %{BASE16NUM}, end: %{BASE16NUM}, len: 3, stride: 8 }
+! [%{BASE16NUM}, %{BASE16NUM}, %{BASE16NUM}]
+! f: %{BASE16NUM}
+! f: %{BASE16NUM}
 ! linked_function
-! f: 0x100c29890
+! f: %{BASE16NUM}
 ! linked_function_2
-! DEBUGGABLES: [1, 2, 0x100c298b8]
+! DEBUGGABLES: [1, 2, %{BASE16NUM}]
 "#);
