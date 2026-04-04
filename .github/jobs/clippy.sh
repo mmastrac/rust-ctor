5fc992e32f1edf6d8efc97f02129e5a2f4c93109
Matt Mastracci <matthew@mastracci.com>
2026-04-04 15:03:53 -0600

    Add build array for linux

--- .github/jobs/clippy.sh
+++ .github/jobs/clippy.sh
@@ -3,6 +3,4 @@ set -euo pipefail
 
 cargo clippy --examples --bins --all -- ${CLIPPY_LINTS}
 
-rustup toolchain add nightly
-rustup component add clippy --toolchain nightly
 cargo +nightly clippy --all-features --examples --bins --all -- ${CLIPPY_LINTS}
