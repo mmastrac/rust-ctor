5cd9a69b37ff80e12cf46dc7636dac311056b02c
Matt Mastracci <matthew@mastracci.com>
2026-04-04 16:27:46 -0600

    Give up on 1.60

--- .github/jobs/build-minimal.sh
+++ .github/jobs/build-minimal.sh
@@ -4,10 +4,6 @@ set -xeuo pipefail
 # Remove Cargo.lock for testing down-level Rust versions
 rm Cargo.lock || true
 
-# libc added a minimum 1.65 which makes it difficult to test
-cargo update -p libc-print --precise 0.1.10
-cargo update -p libc --precise 0.2.96
-
 # Only run the example
 cargo clean
 cargo run -p ctor --example example
