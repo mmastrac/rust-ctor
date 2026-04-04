0f8f3f9096233e7301ee469464437e5af8df0f86
Matt Mastracci <matthew@mastracci.com>
2026-04-04 16:22:58 -0600

    Try a lower libc

--- .github/jobs/build-minimal.sh
+++ .github/jobs/build-minimal.sh
@@ -4,5 +4,5 @@ set -xeuo pipefail
 # Remove Cargo.lock for testing down-level Rust versions
 rm Cargo.lock || true
 cargo clean
-cargo update -p libc --precise 0.1
+cargo update -p libc --precise 0.2.87
 cargo run -p ctor --example example
