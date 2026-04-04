1860e42ac0a626078ea27f0a6a422551ff4e1a04
Matt Mastracci <matthew@mastracci.com>
2026-04-04 17:35:26 -0600

    meta

--- link-section/src/lib.rs
+++ link-section/src/lib.rs
@@ -198,10 +198,10 @@ pub mod __support {
     #[allow(unknown_lints, edition_2024_expr_fragment_specifier)]
     macro_rules! __in_section_crate {
         ($ident:ident generic $section:ident $section_ty:ty, ($(#[$meta:meta])* $vis:vis fn $ident_fn:ident($($args:tt)*) $(-> $ret:ty)? $body:block)) => {
-            $(#[$meta])*
             $crate::__support::section_name!(
                 (
                     // Split the function into a static item and a function pointer
+                    $(#[$meta])*
                     #[no_mangle]
                     #[link_section = __]
                     $vis static $ident_fn: <$section_ty as $crate::__support::SectionItemType>::Item =
@@ -214,16 +214,15 @@ pub mod __support {
             );
         };
         ($ident:ident generic $section:ident $section_ty:ty, ($(#[$meta:meta])* $vis:vis static $ident_static:ident : $ty:ty = $value:expr;)) => {
-            $(#[$meta])*
             $crate::__support::section_name!(
-                (#[no_mangle] #[link_section = __] $vis static $ident_static: <$section_ty as $crate::__support::SectionItemType>::Item = $value;)
+                ($(#[$meta])* #[no_mangle] #[link_section = __] $vis static $ident_static: <$section_ty as $crate::__support::SectionItemType>::Item = $value;)
                 $section section $ident
             );
         };
         ($ident:ident no_generic $section:ident $section_ty:ty, ($(#[$meta:meta])* $vis:vis fn $ident_fn:ident($($args:tt)*) $(-> $ret:ty)? $body:block)) => {
-            $(#[$meta])*
             $crate::__support::section_name!(
                 (
+                    $(#[$meta])*
                     #[no_mangle]
                     #[link_section = __]
                     $vis fn $ident_fn($($args)*) $(-> $ret)? $body
@@ -232,9 +231,8 @@ pub mod __support {
             );
         };
         ($ident:ident no_generic $section:ident $section_ty:ty, ($(#[$meta:meta])* $item:item)) => {
-            $(#[$meta])*
             $crate::__support::section_name!(
-                (#[no_mangle] #[link_section = __] $item)
+                ($(#[$meta])* #[no_mangle] #[link_section = __] $item)
                 $section section $ident
             );
         };
