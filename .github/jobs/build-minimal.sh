a24586ace2313568f26be2bb5d342b8c929c57e8
Matt Mastracci <matthew@mastracci.com>
2026-04-04 16:26:31 -0600

    More MSRV fun

--- .github/jobs/build-minimal.sh
+++ .github/jobs/build-minimal.sh
@@ -3,6 +3,11 @@ set -xeuo pipefail
 
 # Remove Cargo.lock for testing down-level Rust versions
 rm Cargo.lock || true
-cargo clean
+
+# libc added a minimum 1.65 which makes it difficult to test
 cargo update -p libc-print --precise 0.1.10
+cargo update -p libc --precise 0.2.96
+
+# Only run the example
+cargo clean
 cargo run -p ctor --example example
