70d5457a5148f69a255ce0e599ddf4f6fc7bf37d
Matt Mastracci <matthew@mastracci.com>
2026-04-12 13:46:25 -0600

    clean

--- .github/jobs/build.sh
+++ .github/jobs/build.sh
@@ -4,5 +4,6 @@ set -xeuo pipefail
 # Remove Cargo.lock for testing down-level Rust versions
 rm Cargo.lock
 # May need to rebuild when beta/nightly changes
-cargo build || (cargo clean && cargo build)
+cargo clean
+cargo build
 cargo run -p ctor --example example
