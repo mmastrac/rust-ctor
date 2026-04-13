4a2c168ec540215bf3787cf84b6b66eed7501ea5
Matt Mastracci <matthew@mastracci.com>
2026-04-13 09:15:47 -0600

    sanitize all projects

--- .github/jobs/sanitize.sh
+++ .github/jobs/sanitize.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-RUSTFLAGS="-Z sanitizer=address" cargo +nightly run --example example --target $TARGET
+RUSTFLAGS="-Z sanitizer=address" cargo +nightly run -p ctor --example example --target $TARGET
+RUSTFLAGS="-Z sanitizer=address" cargo +nightly run -p link-section --example example --target $TARGET
