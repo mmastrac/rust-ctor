1d225908445d2048843d7a30cd4a2f4a9d7e045a
Matt Mastracci <matthew@mastracci.com>
2026-04-13 09:00:05 -0600

    Add universal target

--- .github/jobs/lint.sh
+++ .github/jobs/lint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-cargo clippy --examples --bins --all -- ${CLIPPY_LINTS}
-cargo fmt --check --all
-
+cargo clippy --examples --bins --all --target $TARGET -- ${CLIPPY_LINTS}
+cargo fmt --check --all --target $TARGET
