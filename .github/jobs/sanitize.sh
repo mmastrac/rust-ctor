3073a5a85f076b65d59aaa21d998beaa34aeb354
Matt Mastracci <matthew@mastracci.com>
2026-04-13 09:17:54 -0600

    example

--- .github/jobs/sanitize.sh
+++ .github/jobs/sanitize.sh
@@ -2,4 +2,4 @@
 set -xeuo pipefail
 
 RUSTFLAGS="-Z sanitizer=address" cargo +nightly run -p ctor --example example --target $TARGET
-RUSTFLAGS="-Z sanitizer=address" cargo +nightly run -p link-section --example example --target $TARGET
+RUSTFLAGS="-Z sanitizer=address" cargo +nightly run -p link-section --example link-section-example --target $TARGET
