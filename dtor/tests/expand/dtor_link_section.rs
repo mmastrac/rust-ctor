e21ddd4726c5691da560e4b7683614d79171528a
Matt Mastracci <matthew@mastracci.com>
2026-04-27 09:57:10 -0600

    explicit method

--- dtor/tests/expand/dtor_link_section.rs
+++ dtor/tests/expand/dtor_link_section.rs
@@ -1,6 +1,6 @@
 use dtor::dtor;
 
-#[dtor(unsafe, link_section = ".dtors", ctor(link_section = ".ctors"))]
+#[dtor(unsafe, method = link_section, link_section = ".dtors", ctor(link_section = ".ctors"))]
 fn foo() {
     println!("foo");
 }
