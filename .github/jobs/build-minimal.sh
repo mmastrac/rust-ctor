6c7f7b35c291c83feacb249e02cefbf8c90e8cad
Matt Mastracci <matthew@mastracci.com>
2026-04-27 11:30:25 -0600

    rename examples

--- .github/jobs/build-minimal.sh
+++ .github/jobs/build-minimal.sh
@@ -4,6 +4,6 @@ set -xeuo pipefail
 # Remove Cargo.lock for testing down-level Rust versions
 rm Cargo.lock || true
 
-# Only run the example
 cargo clean
-cargo run -p ctor --example msrv --target $TARGET
+cargo build
+cargo run -p ctor --example ctor-example --target $TARGET
