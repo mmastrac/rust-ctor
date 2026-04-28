f9f9b8ee6ebf41230801f652163d8a3dab4c9dc2
Matt Mastracci <matthew@mastracci.com>
2026-04-28 15:55:55 -0600

    indent

--- dtor/tests/expand-windows/dtor.expanded.rs
+++ dtor/tests/expand-windows/dtor.expanded.rs
@@ -1,11 +1,11 @@
 use dtor::dtor;
 unsafe fn foo() {
-        unsafe fn __dtor_private_inner() {
-            {
-                ::std::io::_print(format_args!("foo\n"));
-            };
-        }
-        const _: () = {
+    unsafe fn __dtor_private_inner() {
+        {
+            ::std::io::_print(format_args!("foo\n"));
+        };
+    }
+    const _: () = {
         #[link_section = ".CRT$XCU"]
         #[used]
         static __CTOR_PRIVATE_REF: unsafe extern "C" fn() = {
